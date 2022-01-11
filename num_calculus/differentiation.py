99d8538c0067a111075fcb08ccab9eeb71499310
Veera Kaatrasalo <veera.kaatrasalo@gmail.com>
2022-01-11 17:06:06 +0200

    module works

--- num_calculus/differentiation.py
+++ num_calculus/differentiation.py
@@ -1,12 +1,2 @@
-def first_derivative( function, x, dx ):
-    """ Calculates first derivative numerically, with error h^2
-
-    Args:
-        function : function to be detivated
-        x : point in which derivated
-        dx : length of interval
-
-    Returns:
-        numerical value for first derivative
-    """    
+def first_derivative(function, x, dx): 
     return (function(x+dx)-function(x-dx))/(2*dx)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='compphys1',
+      version='0.1',
+      description='numerical first derivative',
+      #url='http://github.com/storborg/funniest',
+      url='https://github.com/verku98/compphys1',
+      author='Veera Kaatrasalo',
+      author_email='veera.kaatrasalo@gmail.com',
+      license='MIT',
+      packages=['num_calculus'],
+      zip_safe=False)
